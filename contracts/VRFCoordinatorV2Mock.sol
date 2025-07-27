06085f8b17911b145c9071eb0921e8a5c164badf
Roopraj591 <2024uma0222@iitjammu.ac.in>
2025-07-27 22:23:10 +0530

    Delete contracts directory

--- contracts/VRFCoordinatorV2Mock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-import "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2Mock.sol";
-
-// Dummy contract just to trigger compilation
-contract CompileHelper is VRFCoordinatorV2Mock {
-    constructor(uint96 _baseFee, uint96 _gasPriceLink) VRFCoordinatorV2Mock(_baseFee, _gasPriceLink) {}
-}
